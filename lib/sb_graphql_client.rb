46b2640069ca958aa74fb3ff4e82224163f5a5a2
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-08-05 14:18:33 +0800

    removed old version of the gemspec and leftover files

--- lib/sb_graphql_client.rb
+++
@@ -1,6 +0,0 @@
-require "sb_graphql_client/version"
-
-module SbGraphqlClient
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/sb_graphql_client/version.rb
+++
@@ -1,3 +0,0 @@
-module SbGraphqlClient
-  VERSION = "0.1.0"
-end
